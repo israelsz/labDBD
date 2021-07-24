937e33915de615c0c7af3f46edff5847d9ed4194
Héctor <hector.ballesteros@usach.cl>
2021-07-24 00:05:31 -0400

    [Héctor Ballesteros] - Creación de modelo y migración de Feedback (Valoración) y PlaylistVideo

---
+++ database/migrations/2021_07_24_035359_create_playlist_videos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlaylistVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+     //TABLA INTERMEDIA PLAYLIST_VIDEO
+    public function up()
+    {
+        Schema::create('playlist_videos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            //Llave foranea con Video
+            $table->unsignedBigInteger('id_video');
+            $table->foreign('id_video')->references('id')->on('videos');
+            //Llave foranea con Playlist
+            $table->unsignedBigInteger('id_playlist');
+            $table->foreign('id_playlist')->references('id')->on('playlists');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('playlist_videos');
+    }
+}
