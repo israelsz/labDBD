c66467f6b27848d929f188a408d59a94508fadc5
Maximiliano Valenzuela <maximiliano.valenzuela.s@usach.cl>
2021-07-24 04:37:14 -0400

    [Maximiliano Valenzuela]-Crear modelo y migraciones de las entidades user, donacion, tipousuario y video_categoria

---
+++ app/Models/donation.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class donation extends Model
+{
+    use HasFactory;
+
+    //Relacion con playlist
+    public function playlist()
+    {
+        return $this->belongsTo(Playlist::class);
+    }
+
+    //Relacion con video
+    public function video(){
+        return $this->belongsTo(Video::class);
+    }
+
+    //Relacion con usuario donador
+    public function donorUser()
+    {
+        return $this->belongsTo(User::class);
+    }
+    //Relacion con usuario receptor
+    public function receivingUser()
+    {
+        return $this->belongsTo(User::class);
+    }
+    //Relacion con metodo pago
+    public function paymentMethod(){
+        return $this->belongsTo(PaymentMethod::class);
+    }
+}
---
+++ database/migrations/2021_07_24_073963_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('password');
+            $table->date('fecha_nacimiento');
+            $table->float('monedero',8,2);
+            $table->string('email')->unique();
+            $table->timestamp('created_at')->nullable();
+            //Llave foranea id_comuna
+            $table->unsignedBigInteger('id_comuna')->nullable();
+            $table->foreign('id_comuna')->references('id')->on('communes');
+            //Llave foranea id_tipo_usuario
+            $table->unsignedBigInteger('id_tipo_usuario')->nullable();
+            $table->foreign('id_tipo_usuario')->references('id')->on('user_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
